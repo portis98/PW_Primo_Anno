1578fc6e70d1236c289b8b13d7171e58037d1d05
Andrea Silvestri <silvestriandrea66@gmail.com>
2025-04-16 18:39:08 +0200

    aggiunto footer e modificato dark mode

--- slide.js
+++ slide.js
@@ -16,11 +16,39 @@ function updateClock() {
     // Lampeggio dei due punti
     const dots = document.getElementById('dots');
     dots.style.visibility = dots.style.visibility === 'hidden' ? 'visible' : 'hidden';
+    dots.style.color = dots.style.color === 'white' ? 'red' : 'white';
 }
 
 // Esegui l'update dell'orologio ogni secondo
 setInterval(updateClock, 1000);
 updateClock(); // Chiama subito all’avvio
+
+
+const modeIcon = document.getElementById("mode-icon");
+const modeText = document.getElementById("mode-text");
+
+// Applica la preferenza salvata al caricamento
+window.onload = function () {
+    const darkMode = localStorage.getItem("darkMode");
+    if (darkMode === "enabled") {
+        document.body.classList.add("dark-mode");
+        modeIcon.textContent = "🌞";
+        modeText.textContent = "Light Mode";
+    }
+};
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+    const isDark = document.body.classList.contains("dark-mode");
+
+    // Cambia icona e testo
+    modeIcon.textContent = isDark ? "🌞" : "🌙";
+    modeText.textContent = isDark ? "Light Mode" : "Dark Mode";
+
+    // Salva preferenza
+    localStorage.setItem("darkMode", isDark ? "enabled" : "disabled");
+}
+
 
 
 
